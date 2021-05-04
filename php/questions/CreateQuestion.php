9a68ed1fc11064e6573a786bb886bfbbbcaea9c6
xkralik <f.kralik96@gmail.com>
2021-05-04 22:23:12 +0200

    added class for creating questions

---
+++ php/questions/CreateQuestion.php
@@ -0,0 +1,27 @@
+<?php
+
+class CreateQuestion {
+
+    public function createSelect($question, $options) {
+        //TODO
+    }
+
+    public function createText($question) {
+        //TODO
+    }
+
+    public function createConnect($question, $optionsA, $optionsB) {
+        //TODO
+    }
+
+    public function createDraw($question) {
+        //TODO
+    }
+
+    public function createEquation($question) {
+        //TODO
+    }
+
+}
+
+?>
