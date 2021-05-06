6dc7330c46f681a104dcee08d71c3243515719cd
xkralik <f.kralik96@gmail.com>
2021-05-06 13:54:37 +0200

    .

---
+++ php/database/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . "/../../config.php";
+
+class Database
+{
+    private $conn;
+
+    /**
+     * Database constructor.
+     * @param $conn
+     */
+    public function __construct()
+    {
+        $this->conn = null;
+        try{
+            $this->conn = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+            $this->conn->exec("set names utf8");
+        }catch (PDOException $exception){
+            echo "Database could not be connected: ".$exception->getMessage();
+        }
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function getConnection(){
+
+        return $this->conn;
+
+    }
+
+
+}
---
+++ php/database/QuestionType.php
@@ -0,0 +1,12 @@
+<?php
+
+
+abstract class QuestionType
+{
+
+    const OPEN = 'open';
+    const CONNECT = 'connect';
+    const CHOOSE = 'choose';
+    const DRAW = 'draw';
+    const MATH = 'math';
+}
