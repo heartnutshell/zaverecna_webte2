c05d5ae3e74d66bc0fe53710364b7682d3df1ba9
xkralik <f.kralik96@gmail.com>
2021-05-06 13:52:17 +0200

    added drawing functionality

---
+++ js/draw.js
@@ -0,0 +1,47 @@
+const initCanvas = (id) => {
+    return new fabric.Canvas(id, {
+        width: 500,
+        height: 500,
+        backgroundColor: 'whitesmoke',
+        isDrawingMode: true,
+    })
+}
+
+const clearCanvas = () => {  
+    canvas.getObjects().forEach((o) => {
+        if(o != canvas.backgroundColor) {
+            canvas.remove(o);
+        }
+    });
+}
+
+const saveDrawing = () => {
+    drawingSave = canvas.toSVG()
+    //need to save into DB
+}
+
+const loadDrawing = () => {
+    clearCanvas();
+    //need to get save from DB
+    if(drawingSave) {
+        fabric.loadSVGFromString(saved, objects => {
+            canvas.add(...objects);
+            canvas.requestRenderAll();
+        });
+    }
+}
+
+const canvas = initCanvas('drawHere');
+const picker = document.getElementById('colorPicker');
+const slider = document.getElementById('sizeSlider');
+let drawingSave;
+
+//set brush color
+picker.onchange = function() {
+    canvas.freeDrawingBrush.color = this.value;
+}
+
+//set brush size
+slider.onchange = function() {
+    canvas.freeDrawingBrush.width = parseInt(this.value, 10);
+}
---
+++ test.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>test</title>
+        <link rel="icon" type="image/png" href="img/favicon.png" />
+        <!-- CSS -->      
+        <!-- JS -->
+        <script src="lib/fabric.min.js"></script>
+        <script src="lib/jscolor.min.js"></script>
+    </head>
+
+    <body>
+
+        <header>
+            <h1>test</h1>
+        </header>
+
+        <div>
+            <input value="#000000" id="colorPicker" data-jscolor="">
+            <input type="range" min="1" max="69" value="1" id="sizeSlider">
+            <button onclick="clearCanvas()" id="clear">Clear</button>
+            <button onclick="saveDrawing()" id="save">Save</button>
+            <button onclick="loadDrawing()" id="save">Load last Save</button>
+            <canvas id="drawHere"></canvas>
+        </div>
+        
+        <footer>           
+            <span>© 2021 HNS</span>
+        </footer>
+
+        <script id="main" src="js/draw.js"></script>  
+
+    </body>
+</html>
