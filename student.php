a53859618a8575480dad84701165c4d9a9cc5792
SingiSvk <73109512+SingiSvk@users.noreply.github.com>
2021-05-15 21:21:12 +0200

    update

--- student.php
+++ student.php
@@ -4,109 +4,130 @@ require_once 'php/database/DatabaseController.php';
 
 $db = new DatabaseController;
 
-    if(isset($_POST['id'])){
-        try {
-                   
-            $id = $_POST['id'];
-            $test = $db->getTestByKey($_POST['test_key']);
-            if($test == NULL){
-                $message = "Zlý kód testu. Test sa nenašiel.";
-                echo "<script type='text/javascript'>alert('$message');</script>";
+if(isset($_POST['id'])){
+    try {
+
+        $id = $_POST['id'];
+        $test = $db->getTestByKey($_POST['test_key']);
+        if($test == NULL || $test[0]['active'] == 0){
+            $message = "Zlý kód testu alebo zadaný test je neaktívny.";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+        } else {
+            $result = $db->getStudentByID($id);
+
+            if ($result == NULL){
+                //treba pridat studenta
+                $name = $_POST['name'];
+                $surname = $_POST['surname'];
+                $db->insertStudent($id, $name, $surname);
+                //mozeme spustit test
             } else {
-                $result = $db->getStudentByID($id);
-
-                if ($result == NULL){
-                    echo 'pridanie noveho studenta';
-                    //treba pridat studenta
-                    $name = $_POST['name'];
-                    $surname = $_POST['surname'];
-                    $db->insertStudent($id, $name, $surname);
-                    //mozeme spustit test
-                } else {
-                    echo 'student uz je';
-                    //student uz existuje netreba ho znovba pridat
-                    //mozeme stupistit test
-                }
-                header("Location: test.php?test_key={$_POST['test_key']}&student_id={$_POST['id']}");
+                //student uz existuje netreba ho znovba pridat
+                //mozeme stupistit test
             }
-
-
-        } catch(PDOException $e) {
-            echo "Error: " . $e->getMessage();
+            header("Location: test.php?test_key={$_POST['test_key']}&student_id={$_POST['id']}");
         }
+
+
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="sk">
 
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Student</title>
-        <link rel="icon" type="image/png" href="img/favicon.png" />
-        <!-- CSS --> 
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">    
-        <!-- JS -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-        <script src="lib/fabric.min.js"></script>
-        <script src="lib/jscolor.min.js"></script>
-    </head>
-    <body>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student</title>
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="lib/fabric.min.js"></script>
+    <script src="lib/jscolor.min.js"></script>
+</head>
+<body>
 
-        <div class="container">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <header>
+            <span class="title">Študent</span>
+        </header>
+        <div class="container-fluid">
 
-            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="form-row">  
-                <div class="form-group col">
-                    <label for='id'>ID</label>            
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="id"
-                        name="id"
-                        required
-                    />
-                </div>               
-                <div class="form-group col">
-                <label for='Name'>Meno</label>                    
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="name"
-                        name="name"
-                        placeholder=""
-                        required
-                    />
-                </div>
-                <div class="form-group col">
-                    <label for='surname'>Priezvisko</label>          
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="surname"
-                        name="surname"
-                        required
-                    />
-                </div>
-                <div class="form-group col">
-                    <label for='test_key'>Kód testu</label>
-                    <input
-                            type="text"
-                            class="form-control"
-                            id="test_key"
-                            name="test_key"
-                            required
-                    />
-                </div>
-                <div class="form-group col">
-                    <input class="btn btn-primary" type="submit" value="Spustiť test!">                        
-                </div>               
-            </form>
 
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <a class="btn btn-secondary my-2 my-sm-0" href="index.php">Späť</a>
+                </form>
+            </div>
         </div>
+    </div>
+</nav>
 
-    </body>
+<div class="container page-content">
+
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="form-row other-center">
+        <div class="form-group col">
+            <label for='id'>ID</label>
+            <input
+                    type="text"
+                    class="form-control"
+                    id="id"
+                    name="id"
+                    required
+            />
+        </div>
+        <div class="form-group col">
+            <label for='Name'>Meno</label>
+            <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    name="name"
+                    placeholder=""
+                    required
+            />
+        </div>
+        <div class="form-group col">
+            <label for='surname'>Priezvisko</label>
+            <input
+                    type="text"
+                    class="form-control"
+                    id="surname"
+                    name="surname"
+                    required
+            />
+        </div>
+        <div class="form-group col">
+            <label for='test_key'>Kód testu</label>
+            <input
+                    type="text"
+                    class="form-control"
+                    id="test_key"
+                    name="test_key"
+                    required
+            />
+        </div>
+        <div class="form-group col"><br>
+            <input class="btn btn-primary" type="submit" value="Spustiť test!">
+        </div>
+    </form>
+
+</div>
+
+<?php include 'footer.php';?>
+
+</body>
 </html>
--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
-include_once "php/questions/GenerateQuestion.php";
+date_default_timezone_set("Europe/Bratislava");
 
+include_once "php/questions/GenerateQuestion.php";
 include_once "php/database/DatabaseController.php";
+
 
 $ctrl = new DatabaseController();
 $generator = new GenerateQuestion();
@@ -14,33 +16,58 @@ $student = $ctrl->getStudentByID($_GET['student_id']);
 $test = $ctrl->getTestByKey($_GET['test_key']);
 $questions = $ctrl->getQuestionsByTestKey($_GET['test_key']);
 
+if($student[0]["name"] != $_POST["name"] || $student[0]["surname"] != $_POST["surname"]){
+    header("location: oops.php?error=wrong_name");
+}
+
+$start_time = $ctrl->getStudentTestsTimestamp($_GET['test_key'], $_GET['student_id']);
+if($start_time) {
+} else {
+    $ctrl->insertStudentTestsTimestamp($_GET['test_key'], $_GET['student_id'], date('Y-m-d H:i:s'));
+    $start_time = $ctrl->getStudentTestsTimestamp($_GET['test_key'], $_GET['student_id']);
+}
+
 ?>
 <html lang="sk">
 
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Registrácia</title>
-        <link rel="icon" type="image/png" href="img/favicon.png" />
-        <!-- CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
-        <!-- JS -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-        <script src="lib/fabric.min.js"></script>
-        <script src="lib/jscolor.min.js"></script>
-        <script src="https://unpkg.com/mathlive/dist/mathlive.js"></script>
-    </head>
-    <body>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="lib/fabric.min.js"></script>
+    <script src="lib/jscolor.min.js"></script>
+    <script src="https://unpkg.com/mathlive/dist/mathlive.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <header>
+            <span class="title">TEST</span>
+        </header>
+
+    </div>
+</nav>
+
+<div class="container page-content">
+
     <?php
-        echo "<form method='post' action='evaluate.php'>";
-        echo "<input type='hidden' name='test_key' value='{$_GET['test_key']}'>";
-        echo "<input type='hidden' name='student_id' value='{$_GET['student_id']}'>";
+    echo "<p id='time_left'></p>";
+    echo "<form method='post' action='evaluate.php' class='test-form' enctype='multipart/form-data'>";
+    echo "<input type='hidden' id='test_key' name='test_key' value='{$_GET['test_key']}'>";
+    echo "<input type='hidden' id='student_id' name='student_id' value='{$_GET['student_id']}'>";
     $ids = "";
-    foreach($questions as $question){
+    foreach($questions as $index => $question){
         $ids = $ids.$question['id'].";";
-        echo "{$question['question']}";
+        $i = $index+1;
+        echo "<h4>$i. {$question['question']}</h4>";
         switch ($question['type']) {
             case "open":
             {
@@ -74,15 +101,23 @@ $questions = $ctrl->getQuestionsByTestKey($_GET['test_key']);
         }
     }
     echo "<input type='hidden' value={$ids} name='ids'>";
-    echo "<input type='submit' class='btn btn-primary' value='Odoslať'>";
+    echo "<input type='submit' id='submit_test' class='btn btn-primary' value='Odoslať'>";
     echo "</form>";
     ?>
 
+</div>
+
+<?php include 'footer.php';?>
+
+<script src="js/draw.js"></script>
+<script src="js/api/studentAnswer.js"></script>
+<script src="js/math.js"></script>
+<script src="js/resize.js"></script>
+<script src="js/countdown.js"></script>
+<script src="js/api/pageVisibility.js"></script>
+
+</body>
 
 
-    <script src="js/draw.js"></script>
-    <script src="js/api/studentAnswer.js"></script>
-    <script src="js/math.js"></script>
 
-    </body>
 </html>
